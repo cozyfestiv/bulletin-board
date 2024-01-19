a3e7a5ec4d7892c1aefee7958564ca80385370ec
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-01-19 17:05:27 -0500

    setting up backend

---
+++ backend/knexfile.js
@@ -0,0 +1,8 @@
+module.exports = {
+  client: 'pg',
+  connection: {
+    connectionString:
+      'postgres://opwsvdkk:5X-oxlTa5r6iB9rgHdcEjJjmL5iavyfO@salt.db.elephantsql.com/opwsvdkk',
+    ssl: { rejectUnauthorized: false }
+  }
+}
