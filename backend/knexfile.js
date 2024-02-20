e097f7b7184947057b9702ff3569ec9730534397
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2024-02-20 17:31:46 -0500

    built express ap p, connected to db

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,8 +1,27 @@
+require('dotenv').config()
+const path = require('path')
+
 module.exports = {
-  client: 'pg',
-  connection: {
-    connectionString:
-      'postgres://opwsvdkk:5X-oxlTa5r6iB9rgHdcEjJjmL5iavyfO@salt.db.elephantsql.com/opwsvdkk',
-    ssl: { rejectUnauthorized: false }
+  development: {
+    client: 'postgresql',
+    pool: { min: 1, max: 2 },
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, 'src', 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'src', 'db', 'seeds')
+    }
+  },
+  production: {
+    client: 'postgresql',
+    pool: { min: 1, max: 2 },
+    connection: process.env.DATABASE_URL_PRODUCTION,
+    migrations: {
+      directory: path.join(__dirname, 'src', 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'src', 'db', 'seeds')
+    }
   }
 }
